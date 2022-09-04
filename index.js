4412bc1a85d586b9bea1e6d9927f8833dea1f27e
nishan das <nishandas880@gmail.com>
2022-09-05 03:38:34 +0600

    token set

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function verifyJwt(req, res, next) {
     if (err) {
       return res.status(403).send({ message: "Forbidden access" });
     }
-    else{
+    else {
       req.decoded = decoded;
       next();
     }
@@ -39,8 +39,8 @@ function verifyJwt(req, res, next) {
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1mscpew.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-async function run(){
- try{
+async function run() {
+  try {
     await client.connect();
     const productsCollection = client.db('inventory').collection('product');
     const ordersCollection = client.db("inventory").collection("orders");
@@ -50,31 +50,26 @@ async function run(){
     const profileCollection = client.db("inventory").collection("profile");
     const blogsCollection = client.db('inventory').collection('blog');
 
-    
 
-    // auth
-    app.post("/login", async (req, res) => {
-      const user = req.body;
-      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "1d",
-      });
-      res.send({ accessToken });
-    });
+
+   
+  
+
     app.get("/products", async (req, res) => {
-        const query = {};
-        const cursor = productsCollection.find(query);
-        const products = await cursor.toArray();
-        res.send(products);
-      });
-    
-    app.get('/products/:id', async(req,res)=>{
-      const id=req.params.id;
-      const query={_id: ObjectId(id)};
+      const query = {};
+      const cursor = productsCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+
+    app.get('/products/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
       const product = await productsCollection.findOne(query);
       res.send(product)
     });
     // reviews area
-    app.get("/reviews", async(req,res) =>{
+    app.get("/reviews", async (req, res) => {
       const query = {};
       const cursor = reviewsCollection.find(query);
       const reviews = await cursor.toArray();
@@ -91,19 +86,47 @@ async function run(){
       const result = await ordersCollection.insertOne(orders);
       res.send({ success: true, result });
     });
+    // order area
+    app.get('/orders/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const orders = await ordersCollection.findOne(query);
+      res.send(orders);
+    });
     // review area
-    
     app.post("/reviews", async (req, res) => {
       const reviews = req.body;
       const result = await reviewsCollection.insertOne(reviews);
       res.send(result);
     });
-      
 
- }
- finally{
-    
- }
+     // put working
+     app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "1h" }
+      );
+      res.send({ result, accessToken: token });
+    });
+
+
+  }
+  finally {
+
+  }
 
 }
 
